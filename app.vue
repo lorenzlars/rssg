57593dba576516ca454134a0679187f7358100ac
Lars Lorenz <mail@larslorenz.dev>
2025-03-28 01:47:58 +0100

    style and config fixes

--- app.vue
+++ app.vue
@@ -1,3 +1,6 @@
+<script lang="ts" setup>
+</script>
+
 <template>
   <n-config-provider>
     <n-loading-bar-provider>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
-import { setAbsoluteSqliteDatabaseUrlForPrisma } from './prisma/utils'
-
-setAbsoluteSqliteDatabaseUrlForPrisma()
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-03-27',
@@ -12,21 +13,42 @@ export default defineNuxtConfig({
   },
 
   modules: [
-    '@nuxtjs/tailwindcss',
     'nuxt-svgo',
-    '@huntersofbook/naive-ui-nuxt',
     '@sidebase/nuxt-auth',
-    '@formkit/nuxt'
+    '@formkit/nuxt',
+    '@nuxt/test-utils',
+    'nuxtjs-naive-ui'
   ],
 
   build: {
     transpile: ['trpc-nuxt']
   },
 
-  extends: ['@sidebase/core'],
+  css: ['~/assets/css/main.css'],
 
   typescript: {
     shim: false
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
   },
 
   auth: {
